f11d6e2eaa2bde88393385e16b6a1e2bf2c70a1b
noedne <bney@andrew.cmu.edu>
2019-12-30 16:48:39 -0500

    added firebase config

---
+++ src/scripts/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBfL5Tw5FzRhqgrYIfSl1-ZIHEWQ2-nUK0',
+  authDomain: 'puyostuco.firebaseapp.com',
+  databaseURL: 'https://puyostuco.firebaseio.com',
+  projectId: 'puyostuco',
+  storageBucket: 'puyostuco.appspot.com',
+  messagingSenderId: '249504518298',
+  appId: '1:249504518298:web:6e704b68850c969785cf23',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
