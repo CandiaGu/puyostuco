5813d9ec64316c156b3381d77d828b311b91aab5
noedne <bney@andrew.cmu.edu>
2020-03-02 17:02:22 -0500

    added constants.js

---
+++ src/constants.js
@@ -0,0 +1,89 @@
+const hwInfo = [
+  {
+    infoText: [
+      'Fill up your SCORE CHALLENGE highscore board!',
+      'Go to PRACTICE > CHALLENGE > SCORE CHALLENGE and play 30 times!',
+    ],
+  },
+  {
+    infoText: [
+      'Practice your stairs!',
+      'Go to LEARN > STAIRS and complete all the drills!',
+    ],
+  },
+  {
+    infoText: [
+      'Practice your sandwich!',
+      'Go to LEARN > SANDWICH and complete all the drills!',
+    ],
+  },
+  {
+    infoText: [
+      'Practice your transition!',
+      'Go to LEARN > TRANSITION and complete all the drills!',
+    ],
+  },
+  {
+    infoText: [
+      'Practice your GTR!',
+      'Go to LEARN > GTR and complete all the drills!',
+    ],
+  },
+  {
+    infoText: [
+      'Practice your tailing!',
+      'Go to LEARN > TAILING and complete all the drills!',
+    ],
+  },
+];
+
+const roomCodes = [
+  'ally',
+  'amitie',
+  'arle',
+  'carbuncle',
+  'ecolo',
+  'klug',
+  'popoi',
+  'raffina',
+  'rafisol',
+  'ringo',
+  'rulue',
+  'satan',
+  'schezo',
+  'sig',
+  'draco',
+  'harpy',
+  'incubus',
+  'kikimora',
+  'lagnus',
+  'minotauros',
+  'nasu grave',
+  'nohoho',
+  'panotty',
+  'serilly',
+  'skeleton t',
+  'suketoudara',
+  'witch',
+  'zoh',
+  'accord',
+  'akuma',
+  'baldanders',
+  'donguri gaeru',
+  'feli',
+  'lidelle',
+  'lemres',
+  'maguro',
+  'ocean prince',
+  'onion pixie',
+  'dapper bones',
+  'rei',
+  'risukuma',
+  'tarutaru',
+  'yu',
+];
+
+export {
+  hwInfo,
+  roomCodes,
+};
